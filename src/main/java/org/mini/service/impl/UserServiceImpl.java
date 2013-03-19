b10ddfb43fd28cb4b07b5cc8c0ec713abb928483
mini-oa <278245180@qq.com>
2013-03-20 00:18:00 +0800

    增加spring的事务控制注解

--- src/main/java/org/mini/service/impl/UserServiceImpl.java
+++ src/main/java/org/mini/service/impl/UserServiceImpl.java
@@ -12,6 +12,8 @@ import org.mini.persist.modules.dao.UserDao;
 import org.mini.service.UserService;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Administrator
@@ -25,24 +27,28 @@ public class UserServiceImpl implements UserService {
 	private UserDao userDao;
 	
 	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void insert(User user) throws Exception {
 		// TODO Auto-generated method stub
 		userDao.insert(user);
 	}
 
 	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void update(User user) throws Exception {
 		// TODO Auto-generated method stub
 		userDao.update(user);
 	}
 
 	@Override
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	public List<User> search(User user) throws Exception {
 		// TODO Auto-generated method stub
 		return userDao.search(user);
 	}
 
 	@Override
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
 	public void delete(long id) throws Exception {
 		// TODO Auto-generated method stub
 		
